d177b5cca610627609de3c6cd25cca8f0092b9ce
Gabriel Amaro <gabriel.amaro94@gmail.com>
2023-10-02 21:57:45 -0300

    fix: fixing script1

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
-#PBS -N mestrado_first_formulation
+#PBS -N main
 #PBS -q paralela
-#PBS -l nodes=8:ppn=128
+#PBS -l nodes=3:ppn=128
 #PBS -e outputs/erros1
 #PBS -o outputs/saidas1
-#PBS -m abe
+#PBS -m ae
 #PBS -M gabriel.amaro94@gmail.com
 
 module load python/3.8.11-intel-2021.3.0
@@ -11,4 +11,4 @@ module load openmpi/4.1.1-intel-2021.3.0
 source py38/bin/activate
 
 cd $PBS_O_WORKDIR
-mpirun python -m mpi4py.futures src/first_formulation.py
+mpirun python -m mpi4py.futures src/main.py
