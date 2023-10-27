19391db5f70d650ebda41c6f1029dded199128ea
Gabriel Amaro <gabriel.amaro94@gmail.com>
2023-10-27 12:15:48 -0300

    removing comments

--- src/main.py
+++ src/main.py
@@ -7,21 +7,21 @@ from first_formulation import build_model as first_reformulation_build_model
 from second_formulation import build_model as second_reformulation_build_model
 
 if __name__ == "__main__":
-    # running_all_instance_choose_capacity(
-    #     classical_formulation_build_model
-    # )
+    running_all_instance_choose_capacity(
+        classical_formulation_build_model
+    )
     running_all_instance_with_chosen_capacity(
         classical_formulation_build_model,
         path_to_save="otimizados_classical.xlsx",
         env_formulation="classical_form",
     )
-    # running_all_instance_with_chosen_capacity(
-    #     first_reformulation_build_model,
-    #     path_to_save="otimizados1.xlsx",
-    #     env_formulation="1st_ref",
-    # )
-    # running_all_instance_with_chosen_capacity(
-    #     second_reformulation_build_model,
-    #     path_to_save="otimizados2.xlsx",
-    #     env_formulation="2nd_ref",
-    # )
+    running_all_instance_with_chosen_capacity(
+        first_reformulation_build_model,
+        path_to_save="otimizados1.xlsx",
+        env_formulation="1st_ref",
+    )
+    running_all_instance_with_chosen_capacity(
+        second_reformulation_build_model,
+        path_to_save="otimizados2.xlsx",
+        env_formulation="2nd_ref",
+    )
