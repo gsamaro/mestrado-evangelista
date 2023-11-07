8461016ec89d6ec648c7b52045503ecf02aa064f
matheusevan33 <matheusevangelista33@gmail.com>
2023-11-06 21:31:05 -0300

    for com todos os experimentos

--- src/main.py
+++ src/main.py
@@ -8,22 +8,23 @@ from first_formulation import build_model as first_reformulation_build_model
 from second_formulation import build_model as second_reformulation_build_model
 
 if __name__ == "__main__":
-    read_experiments("src/experimentos/experimento1.yml")
-    running_all_instance_choose_capacity(
-        classical_formulation_build_model
-    )
-    running_all_instance_with_chosen_capacity(
-        classical_formulation_build_model,
-        path_to_save="otimizados_classical.xlsx",
-        env_formulation="classical_form",
-    )
-    running_all_instance_with_chosen_capacity(
-        first_reformulation_build_model,
-        path_to_save="otimizados1.xlsx",
-        env_formulation="1st_ref",
-    )
-    running_all_instance_with_chosen_capacity(
-        second_reformulation_build_model,
-        path_to_save="otimizados2.xlsx",
-        env_formulation="2nd_ref",
-    )
+    for num in [2,3,4,5,6]:
+        read_experiments(f"src/experimentos/experimento{num}.yml")
+        running_all_instance_choose_capacity(
+            classical_formulation_build_model
+        )
+        running_all_instance_with_chosen_capacity(
+            classical_formulation_build_model,
+            path_to_save="otimizados_classical.xlsx",
+            env_formulation="classical_form",
+        )
+        running_all_instance_with_chosen_capacity(
+            first_reformulation_build_model,
+            path_to_save="otimizados1.xlsx",
+            env_formulation="1st_ref",
+        )
+        running_all_instance_with_chosen_capacity(
+            second_reformulation_build_model,
+            path_to_save="otimizados2.xlsx",
+            env_formulation="2nd_ref",
+        )
